eae6c0714ff09107e811c2123619974a666e5cba
Todd Walraven <cod4247@yahoo.com>
2020-01-31 12:05:57 -0800

    add toppingsTotal prototype

--- js/scripts.js
+++ js/scripts.js
@@ -9,43 +9,51 @@ var purchase = new Pizza (0, 0, 10)
 
 Pizza.prototype.sizeCheck = function(pizza) {
   this.size = pizza;
-  if (this.size === 2) {
+  if (this.size === "Large") {
     this.price += 4
-  } else if (this.size === 1) {
+  } else if (this.size === "Medium") {
     this.price += 2
+  } else if (this.size === "Small") {
+    this.price += 0
   }
 }
 Pizza.prototype.display = function() {
   return this.price + ".00"
 }
 
-Pizza.prototype.addToppings = function() {
-  pizzaToppings += this.toppings 
-  return this.toppings
+Pizza.prototype.addToppings = function(pizza) {
+  this.toppings = pizza;
+  this.topings = totalToppings
 }
+Pizza.prototype.toppingsTotal = function(toppings) {
+  this.toppings = (toppings * 1.5)
+  console.log(this.toppings)
+}
+
 var displaying = function() {
   $("#cost").text(purchase.display());
 };
 var totalToppings = 0
+var pizzaToppings = [];
 $(document).ready(function() { 
   $("form").submit(function(event) {
     event.preventDefault();
-    var pizzaSize = parseInt($("#size").val());
-    var pizzaToppings = [];
+    var pizzaSize = $("#size").val();
+    
     
     $("input:checkbox[name=toppings]:checked").each(function() {
       var toppings = parseFloat($(this).val());
       pizzaToppings.push(toppings);
       
     });
-    pizzaToppings.push(purchase.toppings);
-    $("#output").show();
     purchase.sizeCheck(pizzaSize);
     displaying();
     for (var i= 0; i < pizzaToppings.length; i ++) {
       totalToppings += pizzaToppings[i]
       console.log(totalToppings);
     }
-    
-  })
+    purchase.addToppings(totalToppings);
+    purchase.toppingsTotal(totalToppings);
+    $("#output").show()
+  });
 });
