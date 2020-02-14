b1fb25bba1535cd823599fdc60817dfdb2b0c64a
Todd Walraven <cod4247@yahoo.com>
2020-02-13 21:08:47 -0800

    refactor code to correctly modify pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,50 @@
 // Back End logic
+function Pizza() {
+  this.size = 0,
+  this.toppings = [],
+  this.price = 0
+}
 
-function Pizza(size, toppings, price) {
-  this.size = size,
-  this.toppings = toppings,
-  this.price = price
-}
-Pizza.prototype.pizzaSize = function(pizza) {
-  this.size = pizza;
-}
-Pizza.prototype.startPrice = function() {
-  this.price = 0;
-}
-Pizza.prototype.startToppings = function () {
-  this.toppings = 0;
-}
-Pizza.prototype.addSize = function (pizzaSize) {
+
+Pizza.prototype.setSize = function (pizzaSize) {
   this.size = pizzaSize;
 };
-Pizza.prototype.addToppings = function(pizza) {
-  this.toppings.push(pizza);
-};
-Pizza.prototype.sizeCheck = function (pizza) {
-  this.size = pizza;
-  if (this.size === "Large") {
-    this.price += 15.25
-  } else if (this.size === "Medium") {
-    this.price += 13.25
-  } else if (this.size === "Small") {
-    this.price += 11.25
-  }
-}
-Pizza.prototype.display = function () {
-  return this.price
+
+Pizza.prototype.addTopping = function (topping) {
+  this.toppings.push(topping)
 }
 
-Pizza.prototype.addToppings = function (pizza) {
-  this.toppings = pizza;
-  this.topings = totalToppings
+Pizza.prototype.addPrice = function (price) {
+  this.price += price
 }
-Pizza.prototype.toppingsTotal = function (toppings) {
-  if (this.size === "Large") {
-    this.toppings = (toppings * 2)
-  } else if (this.size === "Medium") {
-    this.toppings = (purchase.toppings * 1.5)
-  } else if (this.size === "small") {
-    this.toppings = (purchase.toppings * 1)
-  }
-}
-Pizza.prototype.grandTotal = function () {
-  this.price += this.toppings
-}
-// Front End Logic below
-var purchase = new Pizza()
-purchase.startPrice();
-purchase.startToppings();
-var displaying = function () {
-  $("#cost").text(purchase.display());
-};
-var totalToppings = 0
-var pizzaToppings = [];
+
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
-    var pizzaSize = $("#size").val();
 
+    var pizza = new Pizza(0, [], 0)
+    var toppingModifier = 1;
+    var basePrice = 11.25;
 
-    $("input:checkbox[name=toppings]:checked").each(function () {
-      var toppings = parseFloat($(this).val());
-      pizzaToppings.push(toppings);
-
-    });
-    purchase.sizeCheck(pizzaSize);
-
-    for (var i = 0; i < pizzaToppings.length; i++) {
-      totalToppings += pizzaToppings[i]
-    }
+    pizza.setSize($("#size").val());
     
-    purchase.pizzaSize(pizzaSize);
-    purchase.addToppings(totalToppings);
-    purchase.toppingsTotal(totalToppings);
-    purchase.grandTotal();
-    displaying();
+    $("input:checkbox[name=toppings]:checked").each(function () {
+      var topping = parseInt($(this).val());
+      pizza.addTopping(topping);
+    });
+
+    console.log(pizza)
+    if (pizza.size === "Medium") {
+      toppingModifier = 1.5;
+      basePrice = 13.25;
+    } else if (pizza.size === "Large") {
+      toppingModifier = 2;
+      basePrice = 15.25;
+    }
+
+    pizza.addPrice(basePrice + pizza.toppings.length * toppingModifier)
+
+    $("#cost").text(pizza.price);
     $("#form-card").hide();
     $("#output").fadeIn();
   });
